0272ef4c7efd73fce2dbf716cc532454a93a3ea6
cchwuya <39360820+cchwuya@users.noreply.github.com>
2018-05-16 23:25:20 -0700

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,10 +1,10 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY")); //这里改成您的api key
-define('GJ_CODE', 'TW');//国家代码
-define('SITE_NAME', 'Youtube代理');//网站名字
-define('TITLENAME', 'Youtube代理');//网站名字
+define('APIKEY', getenv("AIzaSyAsEuZqEEb_4zuzZbjMutAPWzufWc_WqLY ")); //这里改成您的api key
+define('GJ_CODE', 'JP');//国家代码
+define('SITE_NAME', 'pobra');//网站名字
+define('TITLENAME', 'pobra');//网站名字
 define('EN2DEKEY', 'gfw-breaker');//一个随机字符串，加解密KEY
-define('EMAIL', 'lixiaoming0909@gmail.com');//邮箱
-define('PLAYLIST_ID', getenv("PLAYLIST_ID"));//推荐内容播放列表
+define('EMAIL', 'cchwuya@gmail.com');//邮箱
+define('PLAYLIST_ID', getenv("pxSmO5-CtyI"));//推荐内容播放列表
 ?>
